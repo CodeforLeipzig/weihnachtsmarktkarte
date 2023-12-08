bf9dce69bc30241e7e151ea8352f347fb0fc5406
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2023-12-08 17:33:45 +0100

    update Radtanke

--- scraper/markets_leipzig_leben_parser.js
+++ scraper/markets_leipzig_leben_parser.js
@@ -2,62 +2,70 @@ const cheerio = require('cheerio')
 const fs = require('fs')
 
 const getHtml = async (url) => {
-    const res = await fetch(url)
-    return res.text();
+  const res = await fetch(url)
+  return res.text()
 }
-
 
 const scrape = async (content) => {
-    const $ = cheerio.load(content)
-    const events = $('.wp-block-heading')
-    const markets = events.map(function () {
-        const name = $(this).text();
-        const desc = $(this).next()
-        const descTextComplete = desc.text().trim();
-        const whereSegments = descTextComplete.split("Wo? ")
-        const descText = (whereSegments.length > 1) ? whereSegments[0] : descTextComplete;
-        const description = descText.replace("Was? ", "");
-        var nextSegment, location, when;
-        if (whereSegments.length > 1) {
-            nextSegment = desc;
-            const whenSegments = whereSegments[1].split("Wann? ")
-            location = whereSegments[0]
-            if (whenSegments.length > 1) {
-                when = whenSegments[1];
-            }
-        } else {
-            nextSegment = desc.next();
+  const $ = cheerio.load(content)
+  const events = $('.wp-block-heading')
+  const markets = events
+    .map(function () {
+      const name = $(this).text()
+      const desc = $(this).next()
+      const descTextComplete = desc.text().trim()
+      const whereSegments = descTextComplete.split('Wo? ')
+      const descText =
+        whereSegments.length > 1 ? whereSegments[0] : descTextComplete
+      const description = descText.replace('Was? ', '')
+      var nextSegment, location, when
+      if (whereSegments.length > 1) {
+        nextSegment = desc
+        const whenSegments = whereSegments[1].split('Wann? ')
+        location = whereSegments[0]
+        if (whenSegments.length > 1) {
+          when = whenSegments[1]
         }
-        if (!location) {
-            location = nextSegment.text().replace("Wo? ", "")
-            const whenSegments = location.split("Wann? ")
-            if (whenSegments.length > 1) {
-                location = whenSegments[0]
-                when = whenSegments[1];
-            }
-            nextSegment = nextSegment.next();
+      } else {
+        nextSegment = desc.next()
+      }
+      if (!location) {
+        location = nextSegment.text().replace('Wo? ', '')
+        const whenSegments = location.split('Wann? ')
+        if (whenSegments.length > 1) {
+          location = whenSegments[0]
+          when = whenSegments[1]
         }
-        if (!when) {
-            when = nextSegment.text().replace("Wann? ", "")
-            nextSegment = nextSegment.next();
-        }
-        const date = nextSegment.text();
-        const w3c = nextSegment.next().find("a").first().attr("href")
-        return {
-            name,
-            description,
-            location,
-            time: when,
-            date,
-            w3c
-        }
-    }).toArray();
-    const configObj = {
-        markets
-    }
-    fs.writeFileSync('./markets_leipzig_leben.json', JSON.stringify(configObj, null, 2), 'utf-8')
+        nextSegment = nextSegment.next()
+      }
+      if (!when) {
+        when = nextSegment.text().replace('Wann? ', '')
+        nextSegment = nextSegment.next()
+      }
+      const date = nextSegment.text()
+      const w3c = nextSegment.next().find('a').first().attr('href')
+      return {
+        name,
+        description,
+        location,
+        time: when,
+        date,
+        w3c,
+      }
+    })
+    .toArray()
+  const configObj = {
+    markets,
+  }
+  fs.writeFileSync(
+    './markets_leipzig_leben.json',
+    JSON.stringify(configObj, null, 2),
+    'utf-8'
+  )
 }
 
-
-const content = fs.readFileSync("C:/Users/Joerg/Desktop/leipzig-leben.html", 'utf-8')
+const content = fs.readFileSync(
+  '/home/joerg/Schreibtisch/leipzig-leben.html',
+  'utf-8'
+)
 scrape(content)
