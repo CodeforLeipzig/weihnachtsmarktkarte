2ebf523061ed97f82aa3d11276683abedbe94205
Jörg Reichert <joergreichert@users.noreply.github.com>
2025-11-30 01:06:37 +0100

    fix type issue

--- src/lib/hooks/useClickOutside.ts
+++ src/lib/hooks/useClickOutside.ts
@@ -2,8 +2,8 @@ import { RefObject, useEffect, useRef, useState } from 'react'
 
 export default function useClickOutside<RefType extends HTMLElement>(
   callback: EventListener
-): RefObject<RefType> {
-  const container = useRef<RefType>(null)
+): RefObject<RefType|null> {
+  const container = useRef<RefType|null>(null)
   const [isTouchEvent, setTouchEvent] = useState(false)
   const eventType = isTouchEvent ? 'touchend' : 'click'
 
@@ -11,8 +11,8 @@ export default function useClickOutside<RefType extends HTMLElement>(
     function handleEvent(e: Event): void {
       if (e.type === 'click' && isTouchEvent) { return; } // prettier-ignore
 
-      if (container.current && e.target !== null) {
-        if (!container.current.contains(e.target as Node)) {
+      if (container && container.current && e.target !== null) {
+        if (container && !container.current.contains(e.target as Node)) {
           callback(e)
         }
       }
